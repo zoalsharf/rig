d1085e4a97c18464e89fcb00bfb3e9591cfb12aa
zoalsharf <34942644+zoalsharf@users.noreply.github.com>
2020-12-20 09:04:18 -0800

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -13,23 +13,22 @@ chekhosts () {
 }
 
 chek (){
-
-if [ -f "$f1" ]; then
-	for i in ${h[@]}; do
-	sed -i '/'$i'/d' $f1
-	sed -i '1 i\127.0.0.1 '$i'' $f1
-	done
-elif [ -f "$f2" ]; then
-	for i in ${h[@]}; do
-	sed -i '/'$i'/d' $f2
-	sed -i '1 i\127.0.0.1 '$i'' $f2
+	if [ -f "$f1" ]; then
+		for i in ${h[@]}; do
+		sed -i '/'$i'/d' $f1
+		sed -i '1 i\127.0.0.1 '$i'' $f1
 		done
-elif [ -f "$f3" ]; then
-	for i in ${h[@]}; do
-	sed -i '/'$i'/d' $f3
-	sed -i '1 i\127.0.0.1 '$i'' $f3
-	done
-fi
+	elif [ -f "$f2" ]; then
+		for i in ${h[@]}; do
+		sed -i '/'$i'/d' $f2
+		sed -i '1 i\127.0.0.1 '$i'' $f2
+		done
+	elif [ -f "$f3" ]; then
+		for i in ${h[@]}; do
+		sed -i '/'$i'/d' $f3
+		sed -i '1 i\127.0.0.1 '$i'' $f3
+		done
+	fi
 }
 
 chekhosts
